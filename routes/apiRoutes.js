021c847a7250a8325567d54d9f3247c6ca094945
Robert Augusta <RobAAugusta@gmail.com>
2022-02-19 11:44:07 -0500

    Fixing Bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const savedData = require('../db/saveData');
+const savedData = require('../db/savedData');
 
 router.get('/notes', function (req, res) {
-    saveData
+    savedData
         .retrieveNotes()
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
-    saveData
+    savedData
         .addNote(req.body)
         .then((note) => res.json(note))
         .catch(err => res.status(500).json(err));
 });
 
 router.delete('/notes/:id', function (req, res) {
-    saveData
+    savedData
         .deleteNote(req.params.id)
         .then(() => res.json({ ok: true }))
         .catch(err => res.status(500).json(err));
